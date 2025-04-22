7c42efdb0ab292c0d929a2c0425cae3a2bd94ef0
Raman1121 <happy07.dutt@gmail.com>
2025-04-22 14:28:59 +0100

    downstream classification

--- Downstream_Training/run_training.sh
+++ Downstream_Training/run_training.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-source /raid/s2198939/miniconda3/bin/activate sana
-
 export TRAIN_CSV="MIMIC_Splits/LLAVARAD_ANNOTATIONS_TRAIN.csv"
-export IMAGE_DIR="/raid/s2198939/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
+export IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
 
 export MODEL="vit_base_patch16_224.orig_in21k_ft_in1k" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
-export BATCH_SIZE=64
-export EPOCHS=15
+export BATCH_SIZE=512
+export EPOCHS=20
 
-export CUDA_VISIBLE_DEVICES=0,1,2,4
-
-CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python Downstream_Training/train_downstream_classification.py \
-                                            --batch_size=$BATCH_SIZE \
-                                            --epochs=$EPOCHS \
-                                            --model_name=$MODEL \
-                                            --csv_path=$TRAIN_CSV \
-                                            --image_dir=$IMAGE_DIR \
-                                            # --debug
+python Downstream_Training/train_downstream_classification.py \
+        --batch_size=$BATCH_SIZE \
+        --epochs=$EPOCHS \
+        --model_name=$MODEL \
+        --csv_path=$TRAIN_CSV \
+        --image_dir=$IMAGE_DIR \
+        # --debug
