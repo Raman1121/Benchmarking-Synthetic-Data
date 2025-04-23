cfe63705d775430c0b413eccbb8e8ea1ac1b0710
Raman1121 <happy07.dutt@gmail.com>
2025-04-23 17:44:04 +0100

    resnet training

--- Downstream_Training/run_training_inference.sh
+++ Downstream_Training/run_training_inference.sh
@@ -4,9 +4,9 @@ export TRAIN_CSV="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLa
 export TEST_CSV="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/ANNOTATED_CSV_FILES/LLAVARAD_ANNOTATIONS_TEST.csv"
 export IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
 
-export MODEL="vit_base_patch16_224.orig_in21k_ft_in1k" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
+export MODEL="resnet50" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
 export BATCH_SIZE=512
-export EPOCHS=7
+export EPOCHS=10
 
 export EXTRA_INFO="original_data"
 export CHECKPOINT="Downstream_Training/checkpoints/{$MODEL}_best_model.pth"
