530d6706c0ea63c8b40c2a790d66dcb984fb6f29
Raman1121 <happy07.dutt@gmail.com>
2025-04-23 15:02:17 +0100

    generating training data with SD

---
+++ scripts/generate_data_sd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V1_4/IID/512/SD-V1-4_IID_512"
+SAVE_DIR="/pvc/SYNTHETIC_IMAGES/"
+# EXTRA_INFO=""
+
+TEST_PROMPTS_PATH="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/ANNOTATED_CSV_FILES/mimic_train_prompts_20K.txt"
+
+BATCH_SIZE=64
+
+accelerate launch tools/generate_sd.py --model_path $MODEL_PATH \
+                                        --savedir $SAVE_DIR \
+                                        --batch_size $BATCH_SIZE \
+                                        --test_prompts_path $TEST_PROMPTS_PATH
