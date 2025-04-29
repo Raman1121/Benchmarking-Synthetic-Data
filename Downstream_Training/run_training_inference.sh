efa1d8359873af678ed2e2879b62b24807d98e22
Raman1121 <happy07.dutt@gmail.com>
2025-04-29 17:33:22 +0100

    mod

--- Downstream_Training/run_training_inference.sh
+++ Downstream_Training/run_training_inference.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 ## 1. Training on 15K REAL samples (baseline)
-export TRAIN_CSV="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/ANNOTATED_CSV_FILES/training_data_15K_real.csv"
-export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
-export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/Pixart_Sigma"
-export EXTRA_INFO="original_data"
-
-## 2. Training on 15K Synthetic samples
-# export TRAIN_CSV="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/ANNOTATED_CSV_FILES/training_data_15K_synthetic.csv"
+# export TRAIN_CSV="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/ANNOTATED_CSV_FILES/training_data_15K_real.csv"
 # export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
 # export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/Pixart_Sigma"
-# export EXTRA_INFO="syn_data_Pixart_Sigma"
+# export EXTRA_INFO="original_data"
+
+## 2. Training on 15K Synthetic samples
+export TRAIN_CSV="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/ANNOTATED_CSV_FILES/training_data_15K_synthetic.csv"
+export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
+export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/Pixart_Sigma"
+export EXTRA_INFO="syn_data_Pixart_Sigma"
 
 ## 3. Training on 30K Real + Synthetic samples
 # export TRAIN_CSV="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/ANNOTATED_CSV_FILES/training_data_30K_augmented.csv"
@@ -27,9 +27,9 @@ export EPOCHS=10
 
 export CHECKPOINT="Downstream_Training/checkpoints/${MODEL}_${EXTRA_INFO}.pth"
 
-# echo "Training the model..."
-# ./Downstream_Training/run_training.sh > logs_ds_training.txt 2>&1
-# echo "Training Finished!!"
+echo "Training the model..."
+./Downstream_Training/run_training.sh > logs_ds_training.txt 2>&1
+echo "Training Finished!!"
 
 echo "Running inference..."
 ./Downstream_Training/run_inference.sh > logs_ds_inference.txt 2>&1
