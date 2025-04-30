6b76b46ef8cd8b2585087a7c2a538073fae9a82a
Raman1121 <happy07.dutt@gmail.com>
2025-04-30 02:37:37 +0100

    regenerating test data with sd3.5 with bfloat

--- tools/generate_data_sd_3-5_lora.py
+++ tools/generate_data_sd_3-5_lora.py
@@ -75,7 +75,7 @@ def main(args):
     # 4. Device selection
     device = "cuda" if torch.cuda.is_available() else "cpu"
     # dtype = torch.float16 if torch.cuda.is_available() else torch.float32 # Use float16 for efficiency on GPU
-    dtype=torch.bfloat
+    dtype = torch.bfloat16
 
     # --- Check if LoRA file exists ---
     lora_weights_path = os.path.join(lora_weights_dir, lora_weights_filename)
