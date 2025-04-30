71579902fe8cd3d4c80013c99bbef6c52bbb5d33
Raman1121 <happy07.dutt@gmail.com>
2025-04-30 23:31:40 +0100

    data gen again

--- scripts/generate_data_common.sh
+++ scripts/generate_data_common.sh
@@ -109,27 +109,9 @@
 #     --batch_size=$BATCH_SIZE \
 
 #################### Pixart Sigma ####################
-export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/Pixart_diffusers"
-export MODEL_NAME="pixart_sigma"
-export EXTRA_INFO="pixart_sigma"
-
-export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
-export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
-
-export BATCH_SIZE=128
-
-python tools/generate_data_common.py \
-    --model_path=$MODEL_PATH \
-    --model_name=$MODEL_NAME \
-    --extra_info=$EXTRA_INFO \
-    --real_csv=$REAL_CSV \
-    --savedir=$SAVE_DIR \
-    --batch_size=$BATCH_SIZE \
-
-#################### SDV-3.5 LoRA ####################
-# export MODEL_PATH="/pvc/ai-toolkit/output/sd3-5_medium_lora"
-# export MODEL_NAME="SD-V3-5"
-# export EXTRA_INFO="SD-V3-5"
+# export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/Pixart_diffusers"
+# export MODEL_NAME="pixart_sigma"
+# export EXTRA_INFO="pixart_sigma"
 
 # export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
 # export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
@@ -143,6 +125,24 @@ python tools/generate_data_common.py \
 #     --real_csv=$REAL_CSV \
 #     --savedir=$SAVE_DIR \
 #     --batch_size=$BATCH_SIZE \
+
+#################### SDV-3.5 LoRA ####################
+export MODEL_PATH="/pvc/ai-toolkit/output/sd3-5_medium_lora"
+export MODEL_NAME="SD-V3-5"
+export EXTRA_INFO="SD-V3-5"
+
+export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
+export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
+
+export BATCH_SIZE=128
+
+python tools/generate_data_common.py \
+    --model_path=$MODEL_PATH \
+    --model_name=$MODEL_NAME \
+    --extra_info=$EXTRA_INFO \
+    --real_csv=$REAL_CSV \
+    --savedir=$SAVE_DIR \
+    --batch_size=$BATCH_SIZE \
 
 #################### Lumina ####################
 # export MODEL_PATH="/pvc/ai-toolkit/output/lumina2_lora"
