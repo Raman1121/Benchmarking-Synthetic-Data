0f547546bc6c6e013d0ab4c60f5d7d9b1b3d0e39
Raman1121 <happy07.dutt@gmail.com>
2025-04-30 16:53:42 +0100

    downstream eval sana

--- Downstream_Training/run_training_inference.sh
+++ Downstream_Training/run_training_inference.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 ########################################### 1. Training on 15K REAL samples (baseline) ###########################################
-export MODEL="resnet50" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
-export BATCH_SIZE=758
-export TRAIN_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/training_data_15K_real.csv"
-export TEST_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/LLAVARAD_ANNOTATIONS_TEST.csv"
-export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
-export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/RadEdit"
-export EXTRA_INFO="original_data"
-export EPOCHS=10
+# export MODEL="resnet50" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
+# export BATCH_SIZE=758
+# export TRAIN_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/training_data_15K_real.csv"
+# export TEST_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/LLAVARAD_ANNOTATIONS_TEST.csv"
+# export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
+# export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/Sana"
+# export EXTRA_INFO="original_data"
+# export EPOCHS=10
 
-export CHECKPOINT="Downstream_Training/checkpoints/${MODEL}_${EXTRA_INFO}.pth"
+# export CHECKPOINT="Downstream_Training/checkpoints/${MODEL}_${EXTRA_INFO}.pth"
 
 # echo "Training the model..."
 # chmod +x Downstream_Training/run_training.sh
@@ -23,48 +23,48 @@ export CHECKPOINT="Downstream_Training/checkpoints/${MODEL}_${EXTRA_INFO}.pth"
 # echo "Inference Finished!!"
 
 ########################################### 2. Training on 15K Synthetic samples ###########################################
+export MODEL="resnet50" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
+export BATCH_SIZE=758
+export TRAIN_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/training_data_15K_synthetic.csv"
+export TEST_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/LLAVARAD_ANNOTATIONS_TEST.csv"
+export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
+export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/Sana"
+export EXTRA_INFO="syn_data_Sana"
+export EPOCHS=10
+
+export CHECKPOINT="Downstream_Training/checkpoints/${MODEL}_${EXTRA_INFO}.pth"
+
+echo "Training the model..."
+chmod +x Downstream_Training/run_training.sh
+./Downstream_Training/run_training.sh > logs_ds_training_synthetic.txt 2>&1
+echo "Training Finished!!"
+
+echo "Running inference..."
+chmod +x Downstream_Training/run_inference.sh
+./Downstream_Training/run_inference.sh > logs_ds_inference_synthetic.txt 2>&1
+echo "Inference Finished!!"
+
+########################################### 3. Training on 15K Real + Synthetic samples ###########################################
 # export MODEL="resnet50" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
 # export BATCH_SIZE=758
-# export TRAIN_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/training_data_15K_synthetic.csv"
+# export TRAIN_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/training_data_15K_mixed.csv"
 # export TEST_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/LLAVARAD_ANNOTATIONS_TEST.csv"
 # export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
-# export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/RadEdit"
-# export EXTRA_INFO="syn_data_RadEdit"
+# export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/Sana"
+# export EXTRA_INFO="mixed_data_Sana"
 # export EPOCHS=10
 
 # export CHECKPOINT="Downstream_Training/checkpoints/${MODEL}_${EXTRA_INFO}.pth"
 
 # echo "Training the model..."
 # chmod +x Downstream_Training/run_training.sh
-# ./Downstream_Training/run_training.sh > logs_ds_training_synthetic.txt 2>&1
+# ./Downstream_Training/run_training.sh > logs_ds_training_mixed.txt 2>&1
 # echo "Training Finished!!"
 
 # echo "Running inference..."
 # chmod +x Downstream_Training/run_inference.sh
-# ./Downstream_Training/run_inference.sh > logs_ds_inference_synthetic.txt 2>&1
+# ./Downstream_Training/run_inference.sh > logs_ds_inference_mixed.txt 2>&1
 # echo "Inference Finished!!"
-
-########################################### 3. Training on 15K Real + Synthetic samples ###########################################
-export MODEL="resnet50" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
-export BATCH_SIZE=758
-export TRAIN_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/training_data_15K_mixed.csv"
-export TEST_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/LLAVARAD_ANNOTATIONS_TEST.csv"
-export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
-export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/RadEdit"
-export EXTRA_INFO="mixed_data_RadEdit"
-export EPOCHS=10
-
-export CHECKPOINT="Downstream_Training/checkpoints/${MODEL}_${EXTRA_INFO}.pth"
-
-# echo "Training the model..."
-# chmod +x Downstream_Training/run_training.sh
-# ./Downstream_Training/run_training.sh > logs_ds_training_mixed.txt 2>&1
-# echo "Training Finished!!"
-
-echo "Running inference..."
-chmod +x Downstream_Training/run_inference.sh
-./Downstream_Training/run_inference.sh > logs_ds_inference_mixed.txt 2>&1
-echo "Inference Finished!!"
 
 ########################################### 4. Training on 30K Real + Synthetic samples ###########################################
 # export MODEL="resnet50" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
@@ -72,8 +72,8 @@ echo "Inference Finished!!"
 # export TRAIN_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/training_data_30K_augmented.csv"
 # export TEST_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/LLAVARAD_ANNOTATIONS_TEST.csv"
 # export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
-# export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/RadEdit"
-# export EXTRA_INFO="augmented_data_RadEdit"
+# export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES/Sana"
+# export EXTRA_INFO="augmented_data_Sana"
 # export EPOCHS=20
 
 
