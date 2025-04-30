f58ecf20c83d9933b8c62cf8ab1be5403e20731e
Raman1121 <happy07.dutt@gmail.com>
2025-05-01 00:02:08 +0100

    data gen again

--- scripts/generate_data_common.sh
+++ scripts/generate_data_common.sh
@@ -37,9 +37,27 @@
 #     --batch_size=$BATCH_SIZE \
 
 #################### SD-V1-4 ####################
-export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V1_4/IID/512/SD-V1-4_IID_512"
-export MODEL_NAME="SD-V1-4"
-export EXTRA_INFO="SD-V1-4"
+# export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V1_4/IID/512/SD-V1-4_IID_512"
+# export MODEL_NAME="SD-V1-4"
+# export EXTRA_INFO="SD-V1-4"
+
+# export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
+# export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
+
+# export BATCH_SIZE=80
+
+# python tools/generate_data_common.py \
+#     --model_path=$MODEL_PATH \
+#     --model_name=$MODEL_NAME \
+#     --extra_info=$EXTRA_INFO \
+#     --real_csv=$REAL_CSV \
+#     --savedir=$SAVE_DIR \
+#     --batch_size=$BATCH_SIZE \
+
+#################### SD-V1-5 ####################
+export MODEL_PATH="/data/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V1_5/IID/512/SD-V1-5_IID_512"
+export MODEL_NAME="SD-V1-5"
+export EXTRA_INFO="SD-V1-5"
 
 export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
 export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
@@ -54,24 +72,6 @@ python tools/generate_data_common.py \
     --savedir=$SAVE_DIR \
     --batch_size=$BATCH_SIZE \
 
-#################### SD-V1-5 ####################
-# export MODEL_PATH="/data/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V1_5/IID/512/SD-V1-5_IID_512"
-# export MODEL_NAME="SD-V1-5"
-# export EXTRA_INFO="SD-V1-5"
-
-# export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
-# export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
-
-# export BATCH_SIZE=96
-
-# python tools/generate_data_common.py \
-#     --model_path=$MODEL_PATH \
-#     --model_name=$MODEL_NAME \
-#     --extra_info=$EXTRA_INFO \
-#     --real_csv=$REAL_CSV \
-#     --savedir=$SAVE_DIR \
-#     --batch_size=$BATCH_SIZE \
-
 #################### SD-V2 ####################
 # export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V2/IID/512/SD-V2_IID_512"
 # export MODEL_NAME="SD-V2"
@@ -80,7 +80,7 @@ python tools/generate_data_common.py \
 # export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
 # export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
 
-# export BATCH_SIZE=96
+# export BATCH_SIZE=64
 
 # python tools/generate_data_common.py \
 #     --model_path=$MODEL_PATH \
@@ -98,7 +98,7 @@ python tools/generate_data_common.py \
 # export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
 # export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
 
-# export BATCH_SIZE=96
+# export BATCH_SIZE=64
 
 # python tools/generate_data_common.py \
 #     --model_path=$MODEL_PATH \
