6767bda75ef666c52b44389712abef7ebf855530
Raman1121 <happy07.dutt@gmail.com>
2025-05-01 00:16:20 +0100

    data gen again

--- scripts/generate_data_common.sh
+++ scripts/generate_data_common.sh
@@ -37,22 +37,22 @@
 #     --batch_size=$BATCH_SIZE \
 
 #################### SD-V1-4 ####################
-export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V1_4/IID/512/SD-V1-4_IID_512"
-export MODEL_NAME="SD-V1-4"
-export EXTRA_INFO="SD-V1-4"
+# export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V1_4/IID/512/SD-V1-4_IID_512"
+# export MODEL_NAME="SD-V1-4"
+# export EXTRA_INFO="SD-V1-4"
 
-export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
-export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
+# export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
+# export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
 
-export BATCH_SIZE=64
+# export BATCH_SIZE=64
 
-python tools/generate_data_common.py \
-    --model_path=$MODEL_PATH \
-    --model_name=$MODEL_NAME \
-    --extra_info=$EXTRA_INFO \
-    --real_csv=$REAL_CSV \
-    --savedir=$SAVE_DIR \
-    --batch_size=$BATCH_SIZE \
+# python tools/generate_data_common.py \
+#     --model_path=$MODEL_PATH \
+#     --model_name=$MODEL_NAME \
+#     --extra_info=$EXTRA_INFO \
+#     --real_csv=$REAL_CSV \
+#     --savedir=$SAVE_DIR \
+#     --batch_size=$BATCH_SIZE \
 
 #################### SD-V1-5 ####################
 # export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V1_5/IID/512/SD-V1-5_IID_512"
@@ -127,22 +127,22 @@ python tools/generate_data_common.py \
 #     --batch_size=$BATCH_SIZE \
 
 #################### SDV-3.5 LoRA ####################
-# export MODEL_PATH="/pvc/ai-toolkit/output/sd3-5_medium_lora"
-# export MODEL_NAME="SD-V3-5"
-# export EXTRA_INFO="SD-V3-5"
+export MODEL_PATH="/pvc/ai-toolkit/output/sd3-5_medium_lora"
+export MODEL_NAME="SD-V3-5"
+export EXTRA_INFO="SD-V3-5"
 
-# export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
-# export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
+export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
+export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
 
-# export BATCH_SIZE=96
+export BATCH_SIZE=64
 
-# python tools/generate_data_common.py \
-#     --model_path=$MODEL_PATH \
-#     --model_name=$MODEL_NAME \
-#     --extra_info=$EXTRA_INFO \
-#     --real_csv=$REAL_CSV \
-#     --savedir=$SAVE_DIR \
-#     --batch_size=$BATCH_SIZE \
+python tools/generate_data_common.py \
+    --model_path=$MODEL_PATH \
+    --model_name=$MODEL_NAME \
+    --extra_info=$EXTRA_INFO \
+    --real_csv=$REAL_CSV \
+    --savedir=$SAVE_DIR \
+    --batch_size=$BATCH_SIZE \
 
 #################### Lumina ####################
 # export MODEL_PATH="/pvc/ai-toolkit/output/lumina2_lora"
@@ -152,7 +152,7 @@ python tools/generate_data_common.py \
 # export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
 # export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
 
-# export BATCH_SIZE=128
+# export BATCH_SIZE=64
 
 # python tools/generate_data_common.py \
 #     --model_path=$MODEL_PATH \
