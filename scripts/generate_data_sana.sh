b55af123c4a668bac5df946048f2416f67df4d5a
Raman1121 <happy07.dutt@gmail.com>
2025-04-30 14:33:00 +0100

    training data gen with sana

--- scripts/generate_data_sana.sh
+++ scripts/generate_data_sana.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MODEL_PATH="/data/Sana/output Sana_Diffusers/"
+MODEL_PATH="/pvc/Sana/output/Sana_Diffusers/"
 SAVE_DIR="/pvc/SYNTHETIC_IMAGES/"
 EXTRA_INFO="Sana"
 
 TEST_PROMPTS_PATH="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/ANNOTATED_CSV_FILES/mimic_train_prompts_20K.txt"
 
-BATCH_SIZE=64
+BATCH_SIZE=128
 
 accelerate launch tools/generate_sd.py --model_path $MODEL_PATH \
                                         --savedir $SAVE_DIR \
