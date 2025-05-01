7a8a4b08f1447abdb130d02b80a7015ab48eb5c2
Raman1121 <happy07.dutt@gmail.com>
2025-05-01 18:48:37 +0100

    sdv1-5 training data gen

--- scripts/generate_data_common.sh
+++ scripts/generate_data_common.sh
@@ -55,22 +55,22 @@
 #     --batch_size=$BATCH_SIZE \
 
 #################### SD-V1-5 ####################
-# export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V1_5/IID/512/SD-V1-5_IID_512"
-# export MODEL_NAME="SD-V1-5"
-# export EXTRA_INFO="SD-V1-5"
+export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V1_5/IID/512/SD-V1-5_IID_512"
+export MODEL_NAME="SD-V1-5"
+export EXTRA_INFO="SD-V1-5"
 
-# export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
-# export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
+export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
+export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
 
-# export BATCH_SIZE=64
+export BATCH_SIZE=64
 
-# python tools/generate_data_common.py \
-#     --model_path=$MODEL_PATH \
-#     --model_name=$MODEL_NAME \
-#     --extra_info=$EXTRA_INFO \
-#     --real_csv=$REAL_CSV \
-#     --savedir=$SAVE_DIR \
-#     --batch_size=$BATCH_SIZE \
+python tools/generate_data_common.py \
+    --model_path=$MODEL_PATH \
+    --model_name=$MODEL_NAME \
+    --extra_info=$EXTRA_INFO \
+    --real_csv=$REAL_CSV \
+    --savedir=$SAVE_DIR \
+    --batch_size=$BATCH_SIZE \
 
 #################### SD-V2 ####################
 # export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V2/IID/512/SD-V2_IID_512"
