51f4a4b72038057483f6c3b8a5bdbb3a62904c6d
Raman1121 <happy07.dutt@gmail.com>
2025-05-01 02:12:47 +0100

    DS training working fullygit add Downstream_Training/run_training.sh job-train-classification.yaml Downstream_Training/run_training_inference_pixart.sh Downstream_Training/run_training_inference_radedit.sh

--- Downstream_Training/run_training_inference_radedit.sh
+++ Downstream_Training/run_training_inference_radedit.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 ########################################### 1. Training on 20K REAL samples (baseline) ###########################################
-echo "RUNNING EXPERIMENT: ALL REAL DATA"
-export MODEL="resnet50" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
-export T2I_MODEL="radedit"
-export BATCH_SIZE=758
-export TRAIN_CSV="/pvc/SYNTHETIC_IMAGES_NEW/radedit/generations_with_metadata.csv"
-export TEST_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/LLAVARAD_ANNOTATIONS_TEST.csv"
-export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
-export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES_NEW/radedit/generations_with_metadata.csv"
-export TRAINING_SETTING="all_original"
-export EXTRA_INFO=$TRAINING_SETTING
-export EPOCHS=10
+# echo "RUNNING EXPERIMENT: ALL REAL DATA"
+# export MODEL="resnet50" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
+# export T2I_MODEL="radedit"
+# export BATCH_SIZE=758
+# export TRAIN_CSV="/pvc/SYNTHETIC_IMAGES_NEW/radedit/generations_with_metadata.csv"
+# export TEST_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/LLAVARAD_ANNOTATIONS_TEST.csv"
+# export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
+# export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES_NEW/radedit/generations_with_metadata.csv"
+# export TRAINING_SETTING="all_original"
+# export EXTRA_INFO=$TRAINING_SETTING
+# export EPOCHS=10
 
-export CHECKPOINT="Downstream_Training/checkpoints/${MODEL}_${EXTRA_INFO}.pth"
+# export CHECKPOINT="Downstream_Training/checkpoints/${MODEL}_${EXTRA_INFO}.pth"
 
-echo "Training the model..."
-chmod +x Downstream_Training/run_training.sh
-./Downstream_Training/run_training.sh > logs_ds_training_original.txt 2>&1
-echo "Training Finished!!"
+# echo "Training the model..."
+# chmod +x Downstream_Training/run_training.sh
+# ./Downstream_Training/run_training.sh > logs_ds_training_original.txt 2>&1
+# echo "Training Finished!!"
 
-echo "Running inference..."
-chmod +x Downstream_Training/run_inference.sh
-./Downstream_Training/run_inference.sh > logs_ds_inference_original.txt 2>&1
-echo "Inference Finished!!"
+# echo "Running inference..."
+# chmod +x Downstream_Training/run_inference.sh
+# ./Downstream_Training/run_inference.sh > logs_ds_inference_original.txt 2>&1
+# echo "Inference Finished!!"
 
 ########################################### 2. Training on 20K Synthetic samples ###########################################
 echo "RUNNING EXPERIMENT: ALL SYNTHETIC DATA"
