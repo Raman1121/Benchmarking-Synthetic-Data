b7957335eb9e9b6280b1182f8ddb040165ecc9d2
Raman1121 <happy07.dutt@gmail.com>
2025-05-01 01:25:56 +0100

    mod

--- scripts/generate_data_common.sh
+++ scripts/generate_data_common.sh
@@ -73,32 +73,14 @@
 #     --batch_size=$BATCH_SIZE \
 
 #################### SD-V2 ####################
-# export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V2/IID/512/SD-V2_IID_512"
-# export MODEL_NAME="SD-V2"
-# export EXTRA_INFO="SD-V2"
-
-# export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
-# export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
-
-# export BATCH_SIZE=48
-
-# python tools/generate_data_common.py \
-#     --model_path=$MODEL_PATH \
-#     --model_name=$MODEL_NAME \
-#     --extra_info=$EXTRA_INFO \
-#     --real_csv=$REAL_CSV \
-#     --savedir=$SAVE_DIR \
-#     --batch_size=$BATCH_SIZE \
-
-#################### SD-V2-1 ####################
-export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V2_1/IID/512/SD-V2-1_IID_512"
-export MODEL_NAME="SD-V2-1"
-export EXTRA_INFO="SD-V2-1"
+export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V2/IID/512/SD-V2_IID_512"
+export MODEL_NAME="SD-V2"
+export EXTRA_INFO="SD-V2"
 
 export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
 export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
 
-export BATCH_SIZE=48
+export BATCH_SIZE=32
 
 python tools/generate_data_common.py \
     --model_path=$MODEL_PATH \
@@ -107,6 +89,24 @@ python tools/generate_data_common.py \
     --real_csv=$REAL_CSV \
     --savedir=$SAVE_DIR \
     --batch_size=$BATCH_SIZE \
+
+#################### SD-V2-1 ####################
+# export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/OUTPUT_MIMIC_SD_V2_1/IID/512/SD-V2-1_IID_512"
+# export MODEL_NAME="SD-V2-1"
+# export EXTRA_INFO="SD-V2-1"
+
+# export REAL_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/Downstream_Classification_Files/training_data_20K.csv"
+# export SAVE_DIR="/pvc/SYNTHETIC_IMAGES_NEW/"
+
+# export BATCH_SIZE=32
+
+# python tools/generate_data_common.py \
+#     --model_path=$MODEL_PATH \
+#     --model_name=$MODEL_NAME \
+#     --extra_info=$EXTRA_INFO \
+#     --real_csv=$REAL_CSV \
+#     --savedir=$SAVE_DIR \
+#     --batch_size=$BATCH_SIZE \
 
 #################### Pixart Sigma ####################
 # export MODEL_PATH="/pvc/Benchmarking-Synthetic-Data/Pixart_diffusers"
