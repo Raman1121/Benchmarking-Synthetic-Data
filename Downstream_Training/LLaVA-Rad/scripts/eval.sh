cf616e60923933068151f104b058ef4e352e9e29
Raman1121 <happy07.dutt@gmail.com>
2025-05-01 20:17:10 +0100

    llavarad pretraining

--- Downstream_Training/LLaVA-Rad/scripts/eval.sh
+++ Downstream_Training/LLaVA-Rad/scripts/eval.sh
@@ -18,7 +18,8 @@ run_name="${4:-llavarad}"
 query_file=/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/chat_test_MIMIC_CXR_all_gpt4extract_rulebased_v1_SUBSET.json
 image_folder=/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/files
 
-loader="mimic_test_findings"
+# loader="mimic_test_findings"
+loader="default"
 conv_mode="v1"
 
 CHUNKS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
