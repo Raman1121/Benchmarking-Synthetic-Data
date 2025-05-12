b514402e49c4ab1ca87a57830441afeda76e801d
Raman1121 <happy07.dutt@gmail.com>
2025-05-12 20:20:13 +0100

    re-run ds training and eval

--- Downstream_Training/run_training_inference_sdv14.sh
+++ Downstream_Training/run_training_inference_sdv14.sh
@@ -27,9 +27,9 @@
 
 ########################################### 2. Training on 20K Synthetic samples ###########################################
 echo "RUNNING EXPERIMENT: ALL SYNTHETIC DATA"
-export MODEL="resnet50" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
+export MODEL="vit_base_patch16_224.orig_in21k_ft_in1k" #resnet50, vit_base_patch16_224.orig_in21k_ft_in1k
 export T2I_MODEL="SD-V1-4"
-export BATCH_SIZE=758
+export BATCH_SIZE=512
 export TRAIN_CSV="/pvc/SYNTHETIC_IMAGES_NEW/SD-V1-4/generations_with_metadata.csv"
 export TEST_CSV="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/LLAVARAD_ANNOTATIONS_TEST.csv"
 export REAL_IMAGE_DIR="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0"
@@ -37,18 +37,18 @@ export SYNTHETIC_IMG_DIR="/pvc/SYNTHETIC_IMAGES_NEW/SD-V1-4"
 export TRAINING_SETTING="all_synthetic"
 export export EXTRA_INFO=$TRAINING_SETTING
 
-export EPOCHS=20
+export EPOCHS=30
 
 export CHECKPOINT="Downstream_Training/checkpoints/${MODEL}_${EXTRA_INFO}_${T2I_MODEL}.pth"
 
 echo "Training the model..."
 chmod +x Downstream_Training/run_training.sh
-./Downstream_Training/run_training.sh > logs_ds_training_synthetic_${T2I_MODEL}.txt 2>&1
+./Downstream_Training/run_training.sh > logs_ds_training_synthetic_${MODEL}_${T2I_MODEL}.txt 2>&1
 echo "Training Finished!!"
 
 echo "Running inference..."
 chmod +x Downstream_Training/run_inference.sh
-./Downstream_Training/run_inference.sh > logs_ds_inference_synthetic_${T2I_MODEL}.txt 2>&1
+./Downstream_Training/run_inference.sh > logs_ds_inference_synthetic_${MODEL}_${T2I_MODEL}.txt 2>&1
 echo "Inference Finished!!"
 
 ########################################### 3. Training on 20K Real + Synthetic samples ###########################################
