6a69da97b02829c17bac3838a7e5be1a899ccd98
Raman1121 <happy07.dutt@gmail.com>
2025-06-26 23:40:36 +0530

    fixing dtype during data generation

--- tools/generate_data_common.py
+++ tools/generate_data_common.py
@@ -201,9 +201,9 @@ def load_sana_pipeline(model_path, dtype):
     )
 
     if(dtype):
-        print(f"Casting text encoder and transformer to {dtype} dtype.")
+        print(f"Casting text encoder and vae to {dtype} dtype.")
         pipe.text_encoder.to(dtype)
-        pipe.vae = pipe.transformer.to(dtype)
+        pipe.vae.to(dtype)
 
     return pipe
 
